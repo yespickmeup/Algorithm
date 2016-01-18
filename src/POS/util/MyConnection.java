4311901215936cb86dbf27a617cb9a7432ef9ec7
Ronald Pascua <rpascua.synsoftech@gmail.com>
2016-01-18 18:45:25 +0800

    Merge origin/development
    Conflicts:
    	src/tester/NewClass.java

--- src/POS/util/MyConnection.java
+++ src/POS/util/MyConnection.java
@@ -6,7 +6,9 @@ package POS.util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -114,14 +116,10 @@ public class MyConnection {
         }
     }
 
-    public static void main(String[] args) throws SQLException {
-        connect();
-    }
-
     public static Connection cloud_connect() {
         try {
             //        init();
-            String host = System.getProperty("pool_host1", "128.199.80.53:3306");
+            String host = System.getProperty("pool_host1", "128.199.80.531:3306");
             String user = System.getProperty("pool_user1", "root");
             String password = System.getProperty("pool_password1", "password");
             String db_name = System.getProperty("mydb1", "db_algorithm");
@@ -142,4 +140,41 @@ public class MyConnection {
         }
         return conn;
     }
+
+    public static void main(String[] args) {
+        int connected = check_cloud_connection();
+        System.out.println("Is Connected: " + connected);
+    }
+
+    public static int check_cloud_connection() {
+        int connected = 0;
+
+        String host = System.getProperty("pool_host1", "128.199.80.53:3306");
+        String user = System.getProperty("pool_user1", "root");
+        String password = System.getProperty("pool_password1", "password");
+        String db_name = System.getProperty("mydb1", "db_algorithm");
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            String url = "jdbc:mysql://" + host + "/" + db_name;
+            try {
+                Connection conn1 = DriverManager.getConnection(url, user, password);
+                String s0 = "select "
+                        + " id"
+                        + " from users"
+                        + " limit 1";
+                Statement stmt = conn1.createStatement();
+                ResultSet rs = stmt.executeQuery(s0);
+                if (rs.next()) {
+                    connected = 1;
+                }
+            } catch (SQLException ex) {
+                return 0;
+            }
+        } catch (ClassNotFoundException ex) {
+            return 0;
+        }
+        return connected;
+
+    }
+
 }
