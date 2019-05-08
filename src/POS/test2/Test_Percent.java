50b90e8c7c1f0c9be160f545e4f32fc0ef0eee9d
Ronescape <Ronescape@DESKTOP-MM3F6K4>
2019-05-08 23:23:31 +0800

    test

--- src/POS/test2/Test_Percent.java
+++ src/POS/test2/Test_Percent.java
@@ -10,5 +10,19 @@ package POS.test2;
  * @author Ronescape
  */
 public class Test_Percent {
-    
+
+    public static void main(String[] args) {
+
+        double dec = (95 * 100) / 100;
+        dec = dec / 100;
+        System.out.println("Decimal: " + dec);
+
+        double color = 255 * dec;
+        System.out.println("Color: " + color);
+//        for (int i = 1024; i >= 0; i--) {
+//            double percent = (i * 100) / 1024;
+//            System.out.println("Total: " + i + " | " + percent);
+//
+//        }
+    }
 }
