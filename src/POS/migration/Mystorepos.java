997e0dd6a95ae2b19566062cb8763590c788e5a9
Ronescape <Ronescape@DESKTOP-MM3F6K4>
2020-03-11 15:26:28 +0800

    mystorepos

---
+++ src/POS/migration/Mystorepos.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package POS.migration;
+
+/**
+ *
+ * @author Ronescape
+ */
+public class Mystorepos {
+
+    public static void main(String[] args) {
+
+    }
+
+}
